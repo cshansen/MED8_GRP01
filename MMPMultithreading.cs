6a587e5d6529fdb6773b036d2c2a4b360a78420f
Hotpots <ryanandersonucas@gmail.com>
2016-02-25 14:27:20 +0100

    MMPMultithreading
    v1.0

---
+++ MMPMultithreading.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace MMPProject
+{
+    class ThreadTest
+    {
+        static void Main()
+        {
+            Thread t = new Thread(WriteY);          // Kick off a new thread
+            t.Start();                               // running WriteY()
+
+            // Simultaneously, do something on the main thread.
+            for (int i = 0; i < 10000; i++) Console.Write("x");
+            
+        }
+
+        static void WriteY()
+        {
+            for (int i = 0; i < 10000; i++) Console.Write("y");
+        }
+    }
+}
