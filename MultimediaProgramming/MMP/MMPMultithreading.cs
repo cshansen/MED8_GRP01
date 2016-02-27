0f6b810b7f86a95e094d961ce3b9916e46a6184e
Hotpots <ryanandersonucas@gmail.com>
2016-02-27 13:56:43 +0100

    v1.11
    Small changes to description.

--- MultimediaProgramming/MMP/MMPMultithreading.cs
+++ MultimediaProgramming/MMP/MMPMultithreading.cs
@@ -4,6 +4,7 @@ using System.Threading;
 namespace MMPProject
 {
     // Threads share data if they have a common reference to the same object instance.
+    // Using a locker will prevent a thread from entering the IF statement if a thread is currently executing it.
     // A thread, while blocked, doesn't consume CPU resources.
 
 
