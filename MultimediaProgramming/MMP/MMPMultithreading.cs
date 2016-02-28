4e5cfd8ca98cf2b18c4421949a945cf0bf05b787
Hotpots <ryanandersonucas@gmail.com>
2016-02-28 16:36:26 +0100

    v1.12
    More descriptions, regarding thread safety and outputs.

--- MultimediaProgramming/MMP/MMPMultithreading.cs
+++ MultimediaProgramming/MMP/MMPMultithreading.cs
@@ -4,8 +4,10 @@ using System.Threading;
 namespace MMPProject
 {
     // Threads share data if they have a common reference to the same object instance.
-    // Using a locker will prevent a thread from entering the IF statement if a thread is currently executing it.
-    // A thread, while blocked, doesn't consume CPU resources.
+
+    // Using a locker will prevent a thread from entering the IF statement if another thread is currently executing it.
+
+    // A thread, while blocked (using a locker), doesn't consume CPU resources.
 
 
     class ThreadSafe
@@ -23,7 +25,8 @@ namespace MMPProject
         {
             lock (locker)
             {
-                if (!done) { Console.WriteLine("Done"); done = true; }
+                // if (!done) { done = true; Console.WriteLine("Done"); }
+                if (!done) { Console.WriteLine("Done"); done = true; } // Greater chance of printing "Done" twice.
             }
         }
     }
